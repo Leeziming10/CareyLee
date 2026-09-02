48affab7412565f54199f42815f880b1719b9390
Leeziming10 <mfr4frdwtc@privaterelay.appleid.com>
2026-09-03 00:20:59 +0800

    feat: plugins/escpos-lan/Package.swift

---
+++ plugins/escpos-lan/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "BarposEscposLan",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "BarposEscposLan",
+            targets: ["EscposLanPlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.5.1")
+    ],
+    targets: [
+        .target(
+            name: "EscposLanPlugin",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm")
+            ],
+            path: "ios/Sources/EscposLanPlugin")
+    ]
+)
